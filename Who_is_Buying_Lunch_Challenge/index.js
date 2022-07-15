6c8491b2a9b77131a26f049a64602a3379abcc6b
Dra7400 <88469667+Dra7400@users.noreply.github.com>
2022-07-15 14:22:08 -0700

    Create index.js

---
+++ Who_is_Buying_Lunch_Challenge/index.js
@@ -0,0 +1,21 @@
+function whosPaying(names) {
+    
+/******Don't change the code above*******/
+var ranNum = Math.floor(Math.random()*names.length);
+//console.log("ranNum: "+ ranNum);
+
+ var tempNames =(names[ranNum]);
+    // names = ["Angela", "Nicole", "Emre", "Jun", "Kuromi"];
+    // in_names = (names[Math.floor(Math.random()*names.length)]);
+  //  console.log(tempNames);
+    return tempNames +" is going to buy lunch today!";
+    // console.log (whosPaying(names) + "will pay for lunch.")
+    
+    
+    
+
+
+/******Don't change the code below*******/    
+}
+// var names = ["Angela", "Nicole", "Emre", "Jun", "Kuromi"];
+console.log (whosPaying(["Angela", "Nicole", "Mason", "Laura", "Dave"]) );
