1c42588fb6a1d119eb773a39f467851f46759211
Dra7400 <88469667+Dra7400@users.noreply.github.com>
2022-07-19 12:29:31 -0700

    Create index.js

---
+++ Love_Calculator/index.js
@@ -0,0 +1,12 @@
+var name1 = prompt("Please enter the name of a person.");
+var name2 = prompt("Please enter the name of this person's love interest.");
+
+var loveScore = Math.random();
+
+loveScore*=100
+
+loveScore = Math.floor(loveScore)+1
+
+alert("The love score of " name1 + " and " + name2 + " is " + loveScore + "%");
+
+
